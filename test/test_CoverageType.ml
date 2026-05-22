792ecf45867b7ab70ee6ed79c42587918c4574df
Aaryan Prakash <38575855+fishy15@users.noreply.github.com>
2026-05-21 21:14:14 -0400

    Move fast tests into their own file

--- test/test_CoverageType.ml
+++ test/test_CoverageType.ml
@@ -12,20 +12,6 @@ let run_test source_file =
   Printf.printf "passing: %s\n" (List.split_by_comma Fun.id passed);
   Printf.printf "failing: %s\n" (List.split_by_comma Fun.id failed)
 
-let%expect_test "inline_test/alias" =
-  run_test "data/inline_test/alias.ml";
-  [%expect {|
-    passing:
-    failing:
-  |}]
-
-let%expect_test "test_cases/basic_int" =
-  run_test "data/test_cases/basic_int.ml";
-  [%expect {|
-    passing: test1 
-    failing: test3 
-  |}]
-
 (* TODO: fix the test, need definitions for num_arr? *)
 (* let%expect_test "stlc/gen_term_size" = *)
 (*   run_test "data/PLDI23/stlc/gen_term_size.ml"; *)
@@ -43,55 +29,6 @@ let%expect_test "test_cases/basic_int" =
 (*     failing: *)
 (*   |}] *)
 
-let%expect_test "basic/duplicate_list" =
-  run_test "data/PLDI23/basic/duplicate_list.ml";
-  [%expect {|
-    passing: duplicate_list_gen 
-    failing:
-  |}]
-
-let%expect_test "basic/sortedlist_simpl" =
-  run_test "data/PLDI23/basic/sortedlist_simpl.ml";
-  [%expect {|
-    passing: sorted_list_gen 
-    failing:
-  |}]
-
-let%expect_test "basic/boundlist" =
-  run_test "data/PLDI23/basic/boundlist.ml";
-  [%expect {|
-    passing: bound_list_gen 
-    failing:
-  |}]
-
-let%expect_test "quickchick/SizedTree" =
-  run_test "data/PLDI23/quickchick/SizedTree.ml";
-  [%expect {|
-    passing: depth_tree_gen
-    failing:
-  |}]
-
-let%expect_test "quickchick/SizedList" =
-  run_test "data/PLDI23/quickchick/SizedList.ml";
-  [%expect {|
-    passing: sized_list_gen
-    failing:
-  |}]
-
-let%expect_test "quickchick/RedBlackTree" =
-  run_test "data/PLDI23/quickchick/RedBlackTree.ml";
-  [%expect {|
-    passing: rbtree_gen
-    failing:
-  |}]
-
-let%expect_test "quickchick/SortedList" =
-  run_test "data/PLDI23/quickchick/SortedList.ml";
-  [%expect {|
-    passing: sorted_list_gen
-    failing:
-  |}]
-
 (* flaky test *)
 (* let%expect_test "leonidas/SizedBST" = *)
 (*   run_test "data/PLDI23/leonidas/SizedBST.ml"; *)
@@ -99,27 +36,6 @@ let%expect_test "quickchick/SortedList" =
 (*     passing: size_bst_gen *)
 (*     failing: *)
 (*   |}] *)
-
-let%expect_test "leonidas/CompleteTree" =
-  run_test "data/PLDI23/leonidas/CompleteTree.ml";
-  [%expect {|
-    passing: complete_tree_gen
-    failing:
-  |}]
-
-let%expect_test "elrond/BatchedQueue" =
-  run_test "data/PLDI23/elrond/BatchedQueue.ml";
-  [%expect {|
-    passing: batchedq_gen
-    failing:
-  |}]
-
-let%expect_test "elrond/UniqueList" =
-  run_test "data/PLDI23/elrond/UniqueList.ml";
-  [%expect {|
-    passing: unique_list_gen
-    failing:
-  |}]
 
 (* flaky test *)
 (* let%expect_test "elrond/LeftistHeap" = *)
@@ -129,34 +45,6 @@ let%expect_test "elrond/UniqueList" =
 (*     failing: *)
 (*   |}] *)
 
-let%expect_test "elrond/UnbalanceSet" =
-  run_test "data/PLDI23/elrond/UnbalanceSet.ml";
-  [%expect {|
-    passing: unbalanced_set_gen
-    failing:
-  |}]
-
-let%expect_test "elrond/stream" =
-  run_test "data/PLDI23/elrond/stream.ml";
-  [%expect {|
-    passing: stream_gen
-    failing:
-  |}]
-
-let%expect_test "elrond/BankersQueue" =
-  run_test "data/PLDI23/elrond/BankersQueue.ml";
-  [%expect {|
-    passing: bankersq_gen
-    failing:
-  |}]
-
-let%expect_test "quickcheck/SizedHeap" =
-  run_test "data/PLDI23/quickcheck/SizedHeap.ml";
-  [%expect {|
-    passing: depth_heap_gen
-    failing:
-  |}]
-
 (* flaky test *)
 (* let%expect_test "quickcheck/SizedSet" = *)
 (*   run_test "data/PLDI23/quickcheck/SizedSet.ml"; *)
@@ -165,10 +53,10 @@ let%expect_test "quickcheck/SizedHeap" =
 (*     failing: *)
 (*   |}] *)
 
-let%expect_test "alias" =
-  run_test "data/inline_test/alias.ml";
+let%expect_test "quickchick/SortedList" =
+  run_test "data/PLDI23/quickchick/SortedList.ml";
   [%expect {|
-    passing: 
+    passing: sorted_list_gen
     failing:
   |}]
 
@@ -247,10 +135,3 @@ let%expect_test "tezos_test" =
     failing:
   |}]
 *)
-
-let%expect_test "simple/ReturnError" =
-  run_test "data/simple/ReturnError.ml";
-  [%expect {|
-    passing:
-    failing: sized_list_gen
-  |}]
